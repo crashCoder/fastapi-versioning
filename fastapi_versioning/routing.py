38dcaa0d00cc6c86e33eb8be2b811cccf514934e
Antuan Chavez <antuanchavez@gmail.com>
2021-11-10 22:41:55 +0100

    Add support for prefix position and grouping based on the prefix

--- fastapi_versioning/routing.py
+++ fastapi_versioning/routing.py
@@ -4,7 +4,7 @@ from fastapi.routing import APIRoute
 
 
 def versioned_api_route(
-    major: int = 1, minor: int = 0, route_class: Type[APIRoute] = APIRoute
+        major: int = 1, minor: int = 0, prefix: str = "", route_class: Type[APIRoute] = APIRoute
 ) -> Type[APIRoute]:
     class VersionedAPIRoute(route_class):  # type: ignore
         def __init__(self, *args: Any, **kwargs: Any) -> None:
@@ -14,5 +14,11 @@ def versioned_api_route(
             except AttributeError:
                 # Support bound methods
                 self.endpoint.__func__._api_version = (major, minor)
+
+            try:
+                self.endpoint._custom_prefix = prefix
+            except AttributeError:
+                # Support bound methods
+                self.endpoint.__func__._custom_prefix = prefix
 
     return VersionedAPIRoute
